2f2d378a83290a7db194896590f8defe068e4188
IvBrt <crstn.b7@gmail.com>
2022-03-17 20:33:17 -0600

    se muestra sólo última imagen de la data con el for

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import allData from "../data/webdev/webdev.js";
 
-const allDataClone = Object.assign({},allData);
-allDataClone.name = 'Frontend Web Dev Clone';
-console.log(allData,allDataClone);
+const allDataClone = Object.assign({}, allData);
+allDataClone.name = "Frontend Web Dev Clone";
+console.log(allData, allDataClone);
 const arraysTogether = allData.items.concat(allDataClone.items);
-console.log(arraysTogether[0].image);
-
+console.log(arraysTogether);
 
 const App = () => {
-  const el = document.createElement('div');
+  const el = document.createElement("div");
 
-  el.className = 'App';
-  el.innerHTML =`<img src="${arraysTogether[6].image}"></img>`;
-
+  for (let i = 0; i < arraysTogether.length; i++) {
+    console.log(i);
+    let images = arraysTogether[i].image;
+    el.className = "App";
+    el.innerHTML = `
+    <img src="${images}"></img>
+    `;
+    //<h1>${arraysTogether[i].id}</h1>
+  }
 
   return el;
 };
