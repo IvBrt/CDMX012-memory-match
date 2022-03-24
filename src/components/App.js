ca7b6b84bcbd9b4a939344fa9fa3c86c3e89dc19
IvBrt <crstn.b7@gmail.com>
2022-03-23 21:42:43 -0600

    se oculta contenido de cartas con logo de laboratoria

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,26 @@ allDataClone.name = "Frontend Web Dev Clone";
 const bothArr = allData.items.concat(allDataClone.items);
 //console.log(bothArr);
 
-function shuffle (arr){
-  for(let i = arr.length-1; i>0; i--){
-    let h = Math.floor(Math.random()*(i+1));
-    [arr[i],arr[h]]=[arr[h],arr[i]];
-  }
-  console.log(arr)
-}
-
-shuffle(bothArr);
-
 const App = () => {
   const el = document.createElement("div");
 
   for (let i = 0; i < bothArr.length; i++) {
     //console.log(i);
-    el.className = "App";
+    el.className = "cardFront";
     el.innerHTML += `<img src="${bothArr[i].image}">`;
   }
 
   return el;
 };
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let h = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[h]] = [arr[h], arr[i]];
+  }
+  console.log(arr);
+}
+
+shuffle(bothArr);
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+document.getElementById('root').appendChild(App()).style.display = "none";
+
+const imgLab = document.getElementById("lab");
+
+function showCard(){
+    document.getElementById('root').appendChild(App()).style.display = "block";
+    document.querySelector(".cardBack").style.display ="none";
+}
+
+imgLab.addEventListener("click",showCard);
